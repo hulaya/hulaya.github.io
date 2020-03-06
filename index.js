33dfc416f3c7e0da78f055e22ffcbec5ada4f2cf
hulaya <61729107+hulaya@users.noreply.github.com>
2020-03-06 22:14:46 +0800

    框架基本完成
    且待我再添加亿点细节

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
 let clickTime = 0;
 function easterEgg(){
-    if (++clickTime == 10) {
-        document.getElementById('avatar').setAttribute('class', 'easter-Egg-Avatar');
+    if (++clickTime == 12) {
+        document.getElementById('bg').setAttribute('style', 'z-index: 99999;');
+        document.getElementById('name').innerHTML = "快……刷新……页面……！";
+        document.getElementById('avatar').setAttribute('style', 'animation: rotate 1s infinite ease-in-out alternate;')
         clickTime = 0;
+        boHeight = document.getElementById('bg').height / 2;
     }
 }
